7062dcd3f2735a21fd9024de14439974a0848e92
ArifSanaullah <ak7896023@gmail.com>
2020-01-06 15:19:57 +0500

    lesson 2 completed

--- counter-app/src/Counter.js
+++ counter-app/src/Counter.js
@@ -1,42 +1,34 @@
-import React from 'react';
-
-
+import React from "react";
 
 export default class Counter extends React.Component {
-                 constructor(props) {
-                   super(props);
-                   this.state = {
-                     currentNumber: this.props.initialNumber
-                   };
-                 }
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentNumber: this.props.initialNumber
+    };
+  }
 
-                 increment() {
-                   this.setState(prevState => ({
-                     currentNumber: prevState.currentNumber + 1
-                   }));
-                 }
+  increment() {
+    this.setState(prevState => ({
+      currentNumber: prevState.currentNumber + 1
+    }));
+  }
 
-    decrement() {
-        if (this.state.currentNumber > this.props.initialNumber) {
-            this.setState(prevState => ({
-                         
-                currentNumber: prevState.currentNumber - 1,
-            }));
-        }
+  decrement() {
+    if (this.state.currentNumber > this.props.initialNumber) {
+      this.setState(prevState => ({
+        currentNumber: prevState.currentNumber - 1
+      }));
     }
-    
-    
-                 render() {
-                   return (
-                     <div>
-                       <span>{this.state.currentNumber}</span>
-                       <button onClick={() => this.increment()}>
-                         Increment
-                       </button>
-                       <button onClick={() => this.decrement()}>
-                         Decrement
-                       </button>
-                     </div>
-                   );
-                 }
-               }
+  }
+
+  render() {
+    return (
+      <div>
+        <span>{this.state.currentNumber}</span>
+        <button onClick={() => this.increment()}>Increment</button>
+        <button onClick={() => this.decrement()}>Decrement</button>
+      </div>
+    );
+  }
+}
