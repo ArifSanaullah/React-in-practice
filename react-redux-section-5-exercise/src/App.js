bbccc29e179b9c5becd3b392e70525d55a171481
ArifSanaullah <ak7896023@gmail.com>
2020-02-29 02:17:47 +0500

    Section 5 challenge completed.

--- react-redux-section-5-exercise/src/App.js
+++ react-redux-section-5-exercise/src/App.js
@@ -1,18 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Square from './Square'; 
+import React, { Component } from "react";
+import "./App.css";
+import Square from "./Square";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        <Square size={200} color="blue" />
-      </div>
-    );
+    return <Square size={200} color="blue" />;
   }
 }
+
 export default App;
--- react-redux-section-5-exercise/src/Square.js
+++ react-redux-section-5-exercise/src/Square.js
@@ -1,34 +1,61 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './Square.css';
+import React from "react";
+import PropTypes from "prop-types";
+import "./Square.css";
 
 export default class Square extends React.Component {
-    constructor (props) {
-        super(props);
-        this.state = {
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      xAxis: 0,
+      yAxis: 0
+    };
+    this.squareRef = null;
 
-    render () {
-        const size = `${this.props.size}px`;
-        const style = {
-            width: size,
-            height: size,
-            backgroundColor: this.props.initialColor,
-        };
-        return (
-            <div className="SquareContainer">
-                <div style={style} className="Square" />
-            </div>
-        );
-    }
+    this.mouseOverHandler = this.mouseOverHandler.bind(this);
+    this.mouseOutHandler = this.mouseOutHandler.bind(this);
+  }
+
+  mouseOverHandler(e) {
+    const { left, top } = this.squareRef.getBoundingClientRect();
+    this.setState({ xAxis: e.clientX - left, yAxis: e.clientY - top });
+  }
+
+  mouseOutHandler() {
+    this.setState({ xAxis: 0, yAxis: 0 });
+  }
+
+  render() {
+    const size = `${this.props.size}px`;
+    const style = {
+      width: size,
+      height: size,
+      backgroundColor: this.props.initialColor,
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center"
+    };
+    const { xAxis, yAxis } = this.state;
+    return (
+      <div className="SquareContainer">
+        <div
+          ref={e => (this.squareRef = e)}
+          style={style}
+          className="Square"
+          onMouseMove={e => this.mouseOverHandler(e)}
+          onMouseOut={() => this.mouseOutHandler()}
+        >
+          {xAxis}, {yAxis}
+        </div>
+      </div>
+    );
+  }
 }
 
 Square.propTypes = {
-    initialColor: PropTypes.string,
-    size: PropTypes.number.isRequired,
+  initialColor: PropTypes.string,
+  size: PropTypes.number.isRequired
 };
 
 Square.defaultProps = {
-    initialColor: 'blue',
+  initialColor: "blue"
 };
